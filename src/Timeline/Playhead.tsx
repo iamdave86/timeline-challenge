cacd3d30f4652cb1656b6c02122142fcb1fa85f5
iamdave86 <5939607+iamdave86@users.noreply.github.com>
2025-02-26 19:26:37 +0800

    Hide Playhead
    Add 'hidden' class to Playhead when it's out of the view.
    - Left boundary: Hides when position < -(padding + head width)
    - Right boundary: Hides when position > viewport width - (left offset - head width)

--- src/Timeline/Playhead.tsx
+++ src/Timeline/Playhead.tsx
@@ -1,4 +1,9 @@
+import { useMemo } from "react";
 import { useScrollSync } from "./ScrollSync";
+
+const LEFT_OFFSET = 316;
+const HEAD_WIDTH = 5;
+const PADDING = 16; // 1rem
 
 type PlayheadProps = {
   time: number;
@@ -8,9 +13,21 @@ export const Playhead = ({ time }: PlayheadProps) => {
   // scrollLeft is the scroll position of the ruler and keyframe list
   const { scrollLeft } = useScrollSync();
 
+  const isHidden = useMemo(() => {
+    // position is the position of the playhead relative to the scroll position
+    const position = time - scrollLeft;
+
+    // Hide the playhead if it's outside the visible area
+    // left side: position < -(PADDING + HEAD_WIDTH)
+    // right side: position > window.innerWidth - (LEFT_OFFSET - HEAD_WIDTH)
+    return position < -(PADDING + HEAD_WIDTH) || position > window.innerWidth - (LEFT_OFFSET - HEAD_WIDTH);
+  }, [time, scrollLeft]);
+
   return (
     <div
-      className="absolute left-[316px] h-full border-l-2 border-solid border-yellow-600"
+      className={`absolute left-[${LEFT_OFFSET}px] h-full border-l-2 border-solid border-yellow-600 ${
+        isHidden ? "hidden" : ""
+      }`}
       data-testid="playhead"
       style={{ transform: `translateX(calc(${time - scrollLeft}px - 50%))` }}
     >
